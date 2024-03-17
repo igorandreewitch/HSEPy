b2b0ffcbb86051613b2da7e7345c0ea906b5f18b
Igorandreewitch <152905165+igorandreewitch@users.noreply.github.com>
2024-03-17 22:23:47 +0300

    Update Анализ результатов.py

--- Virustotal/Анализ результатов.py
+++ Virustotal/Анализ результатов.py
@@ -7,6 +7,6 @@ with open('otvet\VT2.json', 'r') as file:
 # Фильтруем записи, где свойство "category" равно "malicious"
 filtered_data = [entry['data']['attributes']['last_analysis_results'][key] for entry in data for key in entry['data']['attributes']['last_analysis_results'] if entry['data']['attributes']['last_analysis_results'][key]['category'] == 'malicious']
 
-# Выводим нужные свойства "engine_name", "category" и "result"
+# Выводим свойства "engine_name", "category" и "result"
 for entry in filtered_data:
     print(f"Engine Name: {entry['engine_name']}, Category: {entry['category']}, Result: {entry['result']}")
