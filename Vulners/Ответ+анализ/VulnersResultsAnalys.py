ad1ea2fcd81d19c01586ed565d40360496620e7c
Igorandreewitch <152905165+igorandreewitch@users.noreply.github.com>
2024-03-17 22:26:34 +0300

    Update VulnersResultsAnalys.py

--- Vulners/Ответ+анализ/VulnersResultsAnalys.py
+++ Vulners/Ответ+анализ/VulnersResultsAnalys.py
@@ -2,21 +2,21 @@ import json
 import tkinter as tk
 from tkinter import filedialog
 
-# Открываем файл с данными JSON
+# Открываем JSON
 with open('otvet\\vul\\7\\10.json', 'r') as file:
     data = json.load(file)
 
-# Фильтруем записи по условию, что свойство "cvelist" не пустое
+# Фильтруем записи
 filtered_data = [record for record in data['data']['search'] if record['_source'].get('cvelist')]
 
-# Выводим нужные свойства "title", "cvelist" и "id" для отфильтрованных записей
+# Выводим свойства "title", "cvelist" и "id"
 for record in filtered_data:
     print("Title:", record['_source']['title'])
     print("CVE List:", record['_source']['cvelist'])
     print("ID:", record['_source']['id'])
     print()
 
-# Создаем окно tkinter для выбора места сохранения файла
+# Создаем окно для выбора места сохранения файла
 root = tk.Tk()
 
 # Открываем диалоговое окно для выбора места сохранения файла
